b17e6c712d215229cbc41e7da857b222ba062f86
Christoph Mende <mende.christoph@gmail.com>
2011-06-11 00:48:35 +0200

    Improve build system a bit

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-libtoolize || exit -1
+set -x
+libtoolize --copy || exit -1
 aclocal || exit -1
 autoconf || exit -1
 autoheader || exit -1
-automake || exit -1
+automake --add-missing --copy || exit -1
 
 if [ -z "${NOCONFIGURE}" ]; then
 	./configure
