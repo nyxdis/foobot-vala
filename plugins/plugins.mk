c03fed8a7b9d0371af9abf695085339afd099ee2
Christoph Mende <mende.christoph@gmail.com>
2011-07-24 17:44:59 +0200

    Clean up plugins' autotools

--- plugins/plugins.mk
+++ plugins/plugins.mk
@@ -3,3 +3,6 @@ VALAFLAGS=--pkg foobot \
 	  --vapidir $(top_srcdir)/vapi
 
 AM_CPPFLAGS = -I$(top_srcdir)/include
+LIBADD = $(FOOBOT_LIBS)
+CFLAGS += $(FOOBOT_CFLAGS)
+LDFLAGS += -module -avoid-version
